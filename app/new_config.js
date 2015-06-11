b47a4adb805af4b7e7a85ab2db9b9122521d12b8
Hack Reactor Students <accounts@hackreactor.com>
2015-06-10 20:06:19 -0700

    Mongo Middleware

--- app/new_config.js
+++ app/new_config.js
@@ -1,4 +1,7 @@
 var mongoose = require('mongoose');
+var crypto = require('crypto');
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
 
 if(process.env.NODE_ENV === 'development'){
   mongoose.connect('mongodb://localhost/test');
@@ -17,12 +20,57 @@ var LinkSchema = mongoose.Schema({
 
 var Link = mongoose.model('Link', LinkSchema);
 
+// LinkSchema.methods.initialize = function() {
+//    var shasum = crypto.createHash('sha1');
+//    shasum.update(this.url);
+//    this.code = shasum.digest('hex').slice(0, 5);
+// };
+
+LinkSchema.pre('save', function(next) {
+  console.log("IT WORKS");
+  //console.log(this);
+  var shasum = crypto.createHash('sha1');
+  shasum.update(this.url);
+  this.code = shasum.digest('hex').slice(0, 5);
+  next();
+});
+
+
+var link = new Link({
+  url: 'http://www.google.com',
+  base_url: 'www.google.com',
+  code: 'urlcreated',
+  title: 'Google',
+  visits: 1
+});
+
+
+link.save(function (err) {
+  if (err) {
+    console.log(err);
+  }
+  console.log('new LINK created');
+  console.log(link);
+
+});
+
+LinkSchema.methods.initialize = function() {
+   var shasum = crypto.createHash('sha1');
+   shasum.update(this.url);
+   this.code = shasum.digest('hex').slice(0, 5);
+};
 
 
 var UserSchema = mongoose.Schema({
   username: String,
   password: String
 });
+
+UserSchema.methods.initialize = function() {
+  var cipher = Promise.promisify(bcrypt.hash);
+
+}
+
 
 
 
@@ -33,13 +81,6 @@ var User = mongoose.model('User', UserSchema);
 
 
 
-// var link = new Link({
-//   url: 'http://www.google.com',
-//   base_url: 'www.google.com',
-//   code: 'urlcreated',
-//   title: 'Google',
-//   visits: 1
-// });
 
 
 // link.save(function (err) {
@@ -49,7 +90,7 @@ var User = mongoose.model('User', UserSchema);
 //   console.log('new LINK created');
 //   console.log(link.code);
 
-// });
+//
 
 // var db = mongoose.connection;
 // db.on('error', console.error.bind(console, 'connection error:'));
